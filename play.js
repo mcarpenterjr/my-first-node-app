838ceded9f9e9e4c520867582ff445027a641bde
Mark N Carpenter Jr <mark@mncarpenter.ninja>
2019-02-03 23:25:13 -0500

    INIT

---
+++ play.js
@@ -0,0 +1,16 @@
+const name = 'Max';
+let age = 29;
+const hasHobbies = true;
+
+const summarizeUser = (userName, userAge, userHasHobby) => {
+  return (
+    'Name is ' +
+    userName +
+    ', age is ' +
+    userAge +
+    ' and the user has hobbies: ' +
+    userHasHobby
+  );
+}
+
+console.log(summarizeUser(name, age, hasHobbies));
