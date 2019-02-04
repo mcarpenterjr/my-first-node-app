15fcbaabcd209e45ef5662902950804d34a33f6b
Mark N Carpenter Jr <mark@mncarpenter.ninja>
2019-02-04 00:34:56 -0500

    PROMISES

--- play.js
+++ play.js
@@ -1,19 +1,18 @@
-const person = {
-  name: 'Mark',
-  age: 34,
-  greet() {
-    console.log('Hi, I am ' + this.name)
-  }
-}
+const fetchData = () => {
+  const promise = new Promise((res, rej) => {
+    setTimeout(() => {
+      res('Done!');
+    }, 1500);
+  });
+  return promise;
+};
 
-const printName = ({ name }) => {
-  console.log(name);
-}
-printName(person);
-
-const { name, age } = person;
-console.log(name, age);
-
-const hobbies = ['Cooking', 'Learning', 'Eating', 'Gaming'];
-const [hob1, hob2] = hobbies;
-console.log(hob1, hob2);
+setTimeout(() => {
+  console.log('Timer is Done');
+  fetchData().then(text => {
+    console.log(text);
+    return fetchData();
+  }).then(text => {
+    console.log(text);
+  });
+}, 2000);
