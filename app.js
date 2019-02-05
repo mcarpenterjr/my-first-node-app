462065e23f1cf5e0feddb28e580c26f26f302fc1
Mark N Carpenter Jr <mark@mncarpenter.ninja>
2019-02-04 23:00:31 -0500

    NODEMON support

--- app.js
+++ app.js
@@ -1,40 +1,7 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === '/') {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Get DATA bitch</title></head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">SEND</button></form></body>');
-    res.write('</html>');
-    return res.end();
-  }
-  if (url === '/message' && method === 'POST') {
-    const body = [];
-    req.on('data', (chunk) => {
-      // console.log(chunk);
-      body.push(chunk);
-    });
-    req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
-      // console.log(parsedBody);
-      fs.writeFile('message.txt', message, (err) => {
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-      });
-    });
-  }
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>Get DATA bitch</title></head>');
-  res.write('<body><form><input type="text"><button type="submit"></button></form></body>');
-  res.write('</html>');
-  res.end();
-});
+const routes = require('./routes')
+
+const server = http.createServer(routes);
 
 server.listen(3000);
