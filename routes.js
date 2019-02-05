651c92f7fd7b29d4b2dea082107ec06113af7ba3
Mark N Carpenter Jr <mark@mncarpenter.ninja>
2019-02-04 23:40:21 -0500

    Stupid Mistake

--- routes.js
+++ routes.js
@@ -3,20 +3,21 @@ const fs = require('fs');
 const requestHandler = (req, res) => {
   const url = req.url;
   const method = req.method; 
-  if (url === '/') {
+  if (url === '/home') {
     res.write('<html>');
     res.write('<head><title>Get DATA bitch</title></head>');
     res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">SEND</button></form></body>');
     res.write('</html>');
     return res.end();
   }
+
   if (url === '/message' && method === 'POST') {
     const body = [];
     req.on('data', (chunk) => {
       console.log(chunk);
       body.push(chunk);
     });
-    req.on('end', () => {
+    return req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split('=')[1];
       // console.log(parsedBody);
