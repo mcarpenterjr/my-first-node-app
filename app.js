138665f7371758e7e4aabea64dabf5e5d26a6b3e
Mark N Carpenter Jr <mark@mncarpenter.ninja>
2019-02-04 20:30:26 -0500

    WRITE  FILE on Sub

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-
+  const url = req.url;
+  const method = req.method;
+  if (url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Get DATA bitch</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">SEND</button></form></body>');
+    res.write('</html>');
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'DUMMY');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
-  res.write('<head><title>My PAGE</title></head>');
-  res.write('<body><h1>HELLO!</h1></body>');
+  res.write('<head><title>Get DATA bitch</title></head>');
+  res.write('<body><form><input type="text"><button type="submit"></button></form></body>');
   res.write('</html>');
   res.end();
 });
