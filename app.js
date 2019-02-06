b4e47b65ec4471b2e94125be5d15960d496937a9
Mark N Carpenter Jr <mark@mncarpenter.ninja>
2019-02-06 02:17:16 -0500

    NPM upgrade and Pug

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+// Set Pug as the templste engine
+app.set('view engine', 'pug');
+app.set('view', 'views');
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- routes/admin.js
+++ routes/admin.js
@@ -6,13 +6,19 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
+
+// /admin/add-product GET method
 router.get('/add-product', (req, res, next) => {
   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-router.post('/product', (req, res, next) => {
-  console.log(req.body);
+// /admin/add-product POST method
+router.post('/add-product', (req, res, next) => {
+  // console.log(req.body);
+  products.push({title: req.body.title});
   res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
